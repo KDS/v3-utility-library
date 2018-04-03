d97ee90a7baddaa9a0c0542020c800dc903aa2d6
Vincent FRANCOLIN <vincent.francolin@outlook.com>
2018-04-03 16:12:10 +0200

    fix path in main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 require("./infobox/src/infobox.js");
-require("markerwithlabel/src/markerwithlabel.js");
-require("markerclustererplus/src/markerclusterer.js");
+require("./markerwithlabel/src/markerwithlabel.js");
+require("./markerclustererplus/src/markerclusterer.js");
